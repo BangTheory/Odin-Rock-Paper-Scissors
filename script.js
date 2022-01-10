76ee1e27903611482e7cc6a259a359ce30f7e294
bang <werner.bang@gmail.com>
2022-01-10 03:21:10 -0500

    still need refactoring

--- script.js
+++ script.js
@@ -1,43 +1,74 @@
-function computerPlay(){
-    return Math.floor((Math.random()*3)+1);
-}
+function cpuRoll(){
+     return Math.floor((Math.random()*3)+1);
+ }
 
-function userPrompt(){
-    return prompt("Choose (r)ock, (p)aper, or (s)cissors");
-}
+ let userScore = 0;
+ let cpuScore = 0;
 
 function gamePlay(userP, cpuP){
-    if ((userP == 'r' && cpuP == 1) ||
-        (userP == 'p' && cpuP == 2) ||
-        (userP == 's' && cpuP == 3))
-            return "draw";
-    else if ((userP == "r" && cpuP == 3) ||
-        (userP == "p" && cpuP == 1) ||
-        (userP == "s" && cpuP == 2))
-            return "win";
-    else if ((userP == "r" && cpuP == 2) ||
-        (userP == "p" && cpuP == 3) ||
-        (userP == "s" && cpuP == 1))
-            return "lose";
+    const message = document.getElementById('prompt');
+    const uScore = document.getElementById('player-score');
+    const pScore = document.getElementById('computer-score');
+    if ((userP == 'Rock' && cpuP == 1) ||
+        (userP == 'Paper' && cpuP == 2) ||
+        (userP == 'Scissors' && cpuP == 3)){
+        message.textContent = `${userP} Draws!`;
+        console.log("draw");
+            return;}
+    else if ((userP == "Rock" && cpuP == 3) ||
+        (userP == "Paper" && cpuP == 1) ||
+        (userP == "Scissors" && cpuP == 2)){
+        message.textContent = `${userP} Wins!`;
+        uScore.textContent =`${++userScore}`;
+        console.log("win");
+            return;}
+    else if ((userP == "Rock" && cpuP == 2) ||
+        (userP == "Paper" && cpuP == 3) ||
+        (userP == "Scissors" && cpuP == 1)){
+        message.textContent = `${userP} Loses!`;
+        pScore.textContent =`${++cpuScore}`;
+        console.log("Lose");
+            return;}
 }
 
-let roundNumber = 0;
-let userScore = 0;
-let cpuScore = 0;
+// let roundNumber = 0;
+// let userScore = 0;
+// let cpuScore = 0;
 
-while (roundNumber <= 5){
-    console.log(`Player: ${userScore}, Computer ${cpuScore}, Round: ${roundNumber}\n`);
-    let roundResult = gamePlay(userPrompt(), computerPlay());
-    if (roundResult == "win"){
-        console.log("You Win!");
-        roundNumber++;
-        userScore++;
-    }else if (roundResult == "lose"){
-        console.log("You Lose!");
-        roundNumber++;
-        cpuScore++;
-    }else if (roundResult == "draw"){
-        console.log("Draw!");
-        roundNumber++
-    }
-}
+// while (roundNumber <= 5){
+//     console.log(`Player: ${userScore}, Computer ${cpuScore}, Round: ${roundNumber}\n`);
+//     let roundResult = gamePlay(userPrompt(), computerPlay());
+//     if (roundResult == "win"){
+//         console.log("You Win!");
+//         roundNumber++;
+//         userScore++;
+//     }else if (roundResult == "lose"){
+//         console.log("You Lose!");
+//         roundNumber++;
+//         cpuScore++;
+//     }else if (roundResult == "draw"){
+//         console.log("Draw!");
+//         roundNumber++
+//     }
+// }
+
+// const rock = document.querySelector('.rock');
+// rock.addEventListener('click', userChoice);
+
+// function userChoice(e){
+//     alert(`You picked ${e.srcElement.alt}`);
+//     console.log(e.srcElement);
+// }   
+window.addEventListener('click', function(e){
+    const choice = document.getElementById(`${e.target.alt}`);
+     if(choice == null) return;
+    choice.classList.add('selected');
+    var cpuMove = cpuRoll();
+    console.log(choice.alt);
+    console.log(cpuMove);
+
+    gamePlay(choice.alt, cpuMove);
+
+
+    
+});
